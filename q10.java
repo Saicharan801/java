526d6cf34f1d70e69f0b8b69beaf10567df0d388
Saicharan801 <ithagonicharan9999@gmail.com>
2025-06-05 21:26:44 +0530

    Add files via upload

---
+++ q10.java
@@ -0,0 +1,15 @@
+class C
+{
+     
+	static String m3(int a)
+	{
+	  System.out.print(a);
+	  return "sai";
+	
+	}
+        public static void main(String[]args)
+	{
+	  String result=m3(10);
+	  System.out.print(result);
+	}
+}
